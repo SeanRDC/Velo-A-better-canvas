e661d37b0c78eabea22f2f98de72acf5c47bfb4d
SeanRDC <delacruzseanrhani@gmail.com>
2026-09-23 23:22:20 +0800

    style: added back device preview for ease of testing

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:go_router/go_router.dart';
+import 'package:device_preview/device_preview.dart';
 
 import 'state/app_state.dart';
 import 'theme/app_theme.dart';
@@ -17,7 +18,10 @@ void main() async {
   runApp(
     ChangeNotifierProvider(
       create: (_) => AppState(prefs),
-      child: const VeloApp(),
+      child: DevicePreview(
+        enabled: true,
+        builder: (context) => const VeloApp(),
+      ),
     ),
   );
 }
@@ -25,10 +29,6 @@ void main() async {
 final _router = GoRouter(
   initialLocation: '/',
   routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const LoginScreen(),
-    ),
     GoRoute(
       path: '/',
       builder: (context, state) => const LoginScreen(),
@@ -50,6 +50,8 @@ class VeloApp extends StatelessWidget {
     return MaterialApp.router(
       title: 'Velo: Canvas Co-pilot',
       debugShowCheckedModeBanner: false,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       theme: AppTheme.light,
       darkTheme: AppTheme.dark,
       themeMode: appState.themeMode,
