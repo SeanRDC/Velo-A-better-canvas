6ed8355b137215b1ec74ce92d73db902dd83e90f
SeanRDC <delacruzseanrhani@gmail.com>
2026-09-23 22:59:57 +0800

    chore: created main app layout wrapper, will be adding the side drawer later

---
+++ lib/components/app_shell.dart
@@ -0,0 +1,42 @@
+// Main Application Layout Wrapper
+import 'package:flutter/material.dart';
+import 'package:lucide_icons/lucide_icons.dart';
+import 'bottom_nav.dart';
+
+class AppShell extends StatelessWidget {
+  final String title;
+  final String activeTab;
+  final Widget child;
+  final List<Widget>? actions;
+  final Widget? leading;
+
+  const AppShell({
+    super.key,
+    required this.title,
+    required this.activeTab,
+    required this.child,
+    this.actions,
+    this.leading,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Scaffold(
+      backgroundColor: theme.scaffoldBackgroundColor,
+      appBar: AppBar(
+        title: Text(title),
+        leading: leading ?? IconButton(
+          icon: const Icon(LucideIcons.menu),
+          onPressed: () {
+            // Open Side Drawer added in later update
+          },
+        ),
+        actions: actions,
+      ),
+      body: child,
+      bottomNavigationBar: BottomNav(activeTab: activeTab),
+    );
+  }
+}
