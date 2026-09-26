3c55f212d29716fda4b6ebc316a8ce67813b5c31
SeanRDC <delacruzseanrhani@gmail.com>
2026-09-27 00:16:32 +0800

    feat: wired up inbox screen to main

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ import 'screens/task_detail_screen.dart';
 import 'screens/course_announcements_screen.dart';
 import 'screens/module_item_detail_screen.dart';
 import 'screens/account_screen.dart';
+import 'screens/inbox_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -139,6 +140,10 @@ final _router = GoRouter(
     GoRoute(
       path: '/account',
       builder: (context, state) => const AccountScreen(),
+    ),
+    GoRoute(
+      path: '/inbox',
+      builder: (context, state) => const InboxScreen(),
     ),
   ],
 );
